3ef08234fefbbfe5bc1dd3ec99f71e010efdd5d6
only_samurai <samurai@psych0tik.net>
2013-04-05 19:28:34 -0700

    a touch of code and UI clean-up

--- pearshaped.php
+++ pearshaped.php
@@ -1,24 +1,59 @@
 <?php
-
 error_reporting(E_ALL);
 
-echo "POST: ";
-print_r($_POST);
+// Okay this file is devoid of comments... because explaining what's happening here kinda defeats the point..
+// If you really want to cheat, have a look at the source for ideas.php.... lamer
 
-echo "<br/>------------------------------------------<br />";
-echo "Begin output<br/>";
-echo "<br/>------------------------------------------<br />";
+?>
+<html>
+	<body>
+		<H1> Super basic calculator! </H1>
+		This is a basic calculator.  <br />
+		It does all 4 simple math operations on your inputs.  <br />
+		Input should be integers. <br />
+		<br />
+		<form action='' method='post'>
+			<table>
+				<tr>
+					<td>
+						First number:
+					</td>
+					<td>
+						<input type='text' name='string1' />
+					</td>
+				</tr>
+				<tr>
+					<td>
+						Math'd
+					</td>
+					<td>
+						by:
+					</td>
+				</tr>
+				<tr>
+					<td>
+						Second number:
+					</td>
+					<td>
+						<input type='text' name='string2' />
+					</td>
+				</tr>
+			</table>
+			<input type='submit' name='submit' value='submit' />
+		</form>
+	</body>
+</html>
 
-
+<?php
 
 if(isset($_POST['string1']) && isset($_POST['string2']) && $_POST['string1'] != "" && $_POST['string2'] != "")
 {
 
-	echo "Your inputs were: " . $_POST['string1'] . " and " . $_POST['string2'] . ".<br />";
+	echo "<br />Your inputs were: " . $_POST['string1'] . " and " . $_POST['string2'] . ".<br /><br />";
 
 	if(intval($_POST['string2']) == "zero")
 	{
-		echo "<strong>Are you daft!?!  You can't divide by zero!</strong><br />";
+		die("<strong>Are you daft!?!  You can't divide by zero!</strong><br />");
 	}
 
 
@@ -34,39 +69,3 @@ if(isset($_POST['string1']) && isset($_POST['string2']) && $_POST['string1'] != 
 }
 
 ?>
-
-<html>
-	<body>
-		<H1> Super basic calculator! </H1>
-		This is a basic calculator. It does all 4 simple math operations on your inputs.  Input should be integers. <br />
-		<form action='' method='post'>
-			<table>
-				<tr>
-					<td>
-						String 1:
-					</td>
-					<td>
-						<input type='text' name='string1' />
-					</td>
-				</tr>
-				<tr>
-					<td>
-						Divided
-					</td>
-					<td>
-						by:
-					</td>
-				</tr>
-				<tr>
-					<td>
-						String 2:
-					</td>
-					<td>
-						<input type='text' name='string2' />
-					</td>
-				</tr>
-			</table>
-			<input type='submit' name='submit' value='submit' />
-		</form>
-	</body>
-</html>
